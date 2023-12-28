7d38778f8d65cfddb08c52ca1233bf9e588b0ebb
Divyesh Khuva <75933218+Divyesh1511@users.noreply.github.com>
2023-12-28 21:30:36 +0530

    Update Wildcard-Pattern-Matching.java

--- Wildcard-Pattern-Matching.java
+++ Wildcard-Pattern-Matching.java
@@ -1,1 +1,37 @@
+// Tabulation (DP)
 
+class Solution{
+    static boolean match(String wild, String pattern)
+    {
+        // code here
+        int m = wild.length();
+        int n = pattern.length();
+        
+        if(n == 0) return (m==0);
+        
+        boolean[][] dp = new boolean[n+1][m+1];
+        
+        dp[0][0] = true;
+        
+        for(int j = 1; j < m+1; j++){
+            if(wild.charAt(j-1) == '*'){
+                dp[0][j] = dp[0][j-1];
+            }
+        }
+        
+        
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j  <= m; j++){
+                if(wild.charAt(j-1) == '*'){
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                }else if(wild.charAt(j-1) == '?' || wild.charAt(j-1) == pattern.charAt(i-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }else{
+                    dp[i][j] = false;
+                }
+            }
+        }
+        
+        return dp[n][m];
+    }
+}
