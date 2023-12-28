de802bd455d8ef9ac7d96a072a61b08c10b03ab4
Divyesh Khuva <75933218+Divyesh1511@users.noreply.github.com>
2023-12-28 21:32:13 +0530

    Update Subarray-with-given-sum.java

--- Subarray-with-given-sum.java
+++ Subarray-with-given-sum.java
@@ -1,1 +1,28 @@
-
+class Solution
+{
+    //Function to find a continuous sub-array which adds up to a given number.
+    static ArrayList<Integer> subarraySum(int[] arr, int n, int s) 
+    {
+        // Your code here
+        ArrayList<Integer> res = new ArrayList<>();
+        int sum = 0;
+        int i = 0;
+        for(int j = 0; j < n; j++){
+            sum += arr[j];
+            
+            while(sum > s){
+                sum -= arr[i];
+                i++;
+            }
+            
+            if((sum == s) && i <= j){
+                res.add(i+1);
+                res.add(j+1);
+                return res;
+            }
+        }
+        
+        res.add(-1);
+        return res;
+    }
+}
